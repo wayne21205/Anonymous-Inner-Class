a776f3f3839292bd148f496a95b91bbe87c167e6
waynner <wayne21205@gmail.com>
2015-03-10 13:59:42 +0800

    First commit for all programs

---
+++ src/wayne/test/Zoo.java
@@ -0,0 +1,54 @@
+package wayne.test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+abstract class Animal {
+  abstract Animal speak();
+}
+ 
+
+class Cat extends Animal {
+  public Animal speak() {
+     System.out.println("A");
+     return this;
+  }
+}
+ 
+class Dog extends Animal {
+  public Animal speak() {
+     System.out.println("B");
+     return this;
+  }
+}
+
+
+
+public class Zoo {
+  public static void main(String[] args) {
+	
+	
+    Animal dog = new Dog().speak(); //new子hook為父, 父call會call子 //B
+    Animal cat = new Cat().speak(); //new子hook為父, 父call會call子 //A
+    
+    
+    // Anonymous inner class(AIC)
+    Animal bird = new Animal() { //Override掉 父類的method
+        Animal speak() {
+            System.out.println("C");
+             return this;
+        }
+    }.speak(); //C
+    
+    
+    // Ex 2
+    //目前時間
+    Date date = new Date();
+    //設定日期格式
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    //進行轉換
+    String dateString = sdf.format(date);
+    System.out.println(dateString);
+    
+  }
+}
